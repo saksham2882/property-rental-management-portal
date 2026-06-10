496f5c3c29d821ba39aafd09d807f0656eb376e0
Khushi-Ranjan <khushiranjan2004@gmail.com>
2026-06-10 22:32:05 +0530

    feat: add shared directives and validators with auth updates

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -8,8 +8,8 @@ import { AuthService } from '../../../core/services/auth-service';
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterLink],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './login.html',
+  styleUrl: './login.css'
 })
 export class LoginComponent {
 
