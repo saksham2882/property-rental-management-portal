f76f7974c946107aadef3c11ffa9db1a78616e0e
Sa-hil36 <sahilmadeshiya36@gmail.com>
2026-06-11 10:22:01 +0530

    modify models name

--- src/app/core/services/lease-service.ts
+++ src/app/core/services/lease-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Lease } from '../models/lease.model';
+import { Lease } from '../models/lease-model';
 
 @Injectable({ providedIn: 'root' })
 export class LeaseService {
