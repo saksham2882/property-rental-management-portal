b8a3c5b560a3bcb280c9587465f53ded45eae088
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-06-11 11:34:43 +0530

    implemented the notification feature and integrated with models

--- src/app/features/customer/notifications/notifications.ts
+++ src/app/features/customer/notifications/notifications.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotificationService } from '../../../core/services/notification.service';
+import {Notification} from '../../../core/models/notification-model';
+
+// interface Notification {
+//   id: number;
+//   title?: string;
+//   message?: string;
+//   isRead: boolean;
+//   createdAt?: string;
+// }
 
 @Component({
   selector: 'app-notifications',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './notifications.html',
   styleUrl: './notifications.css',
 })
-export class Notifications {}
+export class NotificationsComponent implements OnInit {
+
+  notifications: Notification[] = [];
+
+  constructor(
+    private notificationService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadNotifications();
+  }
+
+  loadNotifications(): void {
+    const userId = 1; 
+
+    this.notificationService
+      .getByUser(userId)
+      .subscribe({
+        next: (data) => {
+          this.notifications = data;
+        },
+        error: (err) => {
+          console.error('Failed to load notifications', err);
+        }
+      });
+  }
+
+  markAsRead(id: number): void {
+    this.notificationService
+      .markAsRead(id)
+      .subscribe(() => {
+        this.notifications = this.notifications.map(notification =>
+          notification.id === id
+            ? { ...notification, isRead: true }
+            : notification
+        );
+      });
+  }
+
+  getUnreadCount(): number {
+    return this.notifications.filter(n => !n.isRead).length;
+  }
+}
