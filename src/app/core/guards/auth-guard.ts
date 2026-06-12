76c620c124532492c0b98c2bb734bdabc192aef1
Saksham Agrahari <agrahari0899@gmail.com>
2026-06-12 10:30:51 +0530

    (feat): add customer guard and fix footer

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -2,15 +2,19 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth-service';
 
-export const authGuard: CanActivateFn = () => {
-
+export const redirectIfLoggedInGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
-  if(auth.isLoggedIn()){
-    return true;
+  if (auth.isLoggedIn()) {
+    if (auth.isAdmin()) {
+      router.navigate(['/admin/dashboard']);
+    } else {
+      router.navigate(['/customer/dashboard']);
+    }
+    return false;
   }
 
-  router.navigate(['/auth/login']);
-  return false;
+  return true;
 };
+
